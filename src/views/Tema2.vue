5dc44fd0b94c0bff69dbfd9243eb5b1a31196466
Francisco Lizcano <flizcanor@icloud.com>
2022-09-27 23:56:19 -0500

    Update Tema2.vue

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -30,7 +30,73 @@
         p.mb-0 Al realizar la auditoría y detectar los diferentes tipos de riesgos y vulnerabilidades a los que las organizaciones están expuestas, inicia el funcionamiento de las fases de implementación de las políticas de seguridad de la información. Toda política que se diseñe debe salir en consenso entre los auditores y directivas de la organización y debe cumplirse y monitorearse, con el fin de controlar y mejorar día a día. Las políticas dan garantías a la organización, sus trabajadores y, sobre todo, confianza a sus clientes.
 
 
+    .bg_img_01.my-5(data-aos="zoom-in")
+      .row.justify-content-center.align-items-center
+        .col-lg-5.col-md-8
+          p.mb-4 Dentro de las fases de implementación de un SGSI, están:
 
+          ul.lista-ul--color
+            li.mb-2
+              i.fas.fa-database
+              | Comunicación interna.
+
+            li.mb-2
+              i.fas.fa-database
+              | Establecimiento del equipo de implementación.
+
+            li.mb-2
+              i.fas.fa-database
+              | Estado actual de la organización.
+
+            li.mb-2
+              i.fas.fa-database
+              | Plan de acción.
+
+            li.mb-2
+              i.fas.fa-database
+              | Implementación y operación.
+
+            li.mb-2
+              i.fas.fa-database
+              | Evaluación.
+
+            li.mb-2
+              i.fas.fa-database
+              | Mejora continua.
+
+            li.mb-2
+              i.fas.fa-database
+              | Certificación.
+
+            li.mb-2
+              i.fas.fa-database
+              | Liderazgo.
+
+            li
+              i.fas.fa-database
+              | Riesgos.
+
+
+
+        .col-lg-5.col.md-4.d-none.d-lg-block
+          figure.px-lg-5.mx-lg-5
+            img(src="@/assets/curso/temas/t2/img3.png", alt="Imagen decorativa")
+
+    .tarjeta.color-primario.p-3.mb-5
+      .row.justify-content-around.align-items-center
+        .col-3.col-sm-2.col-lg-1
+          img(src="@/assets/curso/temas/t2/img4.svg")
+        .col
+          .row.justify-content-between.align-items-center
+            .col.mb-3.mb-sm-0
+              p.texto-blanco.text-small.mb-4 Para mayor ilustración, se solicita al aprendiz consultar la guía del MinTIC (Ministerio de Tecnologías de la Información y las Comunicaciones de Colombia).
+
+              p.texto-blanco.text-small #[strong Modelo de Seguridad y Privacidad de la Información – Seguridad y Privacidad de la Información (2016).]
+
+            .col-sm-auto
+              a.boton.color-acento-botones.texto-blanco(href="https://www.mintic.gov.co/gestionti/615/articles-5482_Modelo_de_Seguridad_Privacidad.pdf" target="_blank")
+                span Ir a sitio web
+                i.fas.fa-link
 </template>
 
 <script>
@@ -50,4 +116,7 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.bg-gradiente-2
+  background: linear-gradient(to bottom, #7433EE, #3C1B7B);
+</style>
